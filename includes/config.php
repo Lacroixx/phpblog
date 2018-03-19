1575617cca3037416a458940aab5216c6cde1c18
Ivan Molotov <facelessvoid14885@gmail.com>
2018-03-19 21:00:47 +0200

    first commit

---
+++ includes/config.php
@@ -0,0 +1,14 @@
+<?php
+
+ $config = array(
+    'title' => 'All about RPG',
+    'vk_url' => 'https://vk.com/id111230350',
+    'db' => array('server' => 'localhost',
+                  'username' => 'Overlord',
+                  'password' => '1234rfvb',
+                  'name' => 'newblog'
+
+    )
+ );
+
+ require "db.php";
---
+++ index.php
@@ -0,0 +1,177 @@
+﻿<?php
+require "includes/config.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title><?php echo $config['title'] ?></title>
+
+  <!-- Bootstrap Grid -->
+  <link rel="stylesheet" type="text/css" href="media/assets/bootstrap-grid-only/css/grid12.css">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+
+  <!-- Custom -->
+  <link rel="stylesheet" type="text/css" href="media/css/style.css">
+</head>
+<body>
+
+  <div id="wrapper">
+
+    <?php include "includes/header.php"?>
+
+    <div id="content">
+      <div class="container">
+        <div class="row">
+          <section class="content__left col-md-8">
+            <div class="block">
+              <a href="articles.php">Все записи</a>
+              <h3>Последние статьи</h3>
+              <div class="block__content">
+                <div class="articles articles__horizontal">
+                    <?php $articles = mysqli_query($connection, "SELECT * FROM `articles` ORDER BY `pubdate` DESC LIMIT 4");
+
+                    while($art = mysqli_fetch_assoc($articles))
+                    {
+                        ?>
+
+                        <article class="article">
+                            <div class="article__image" style="background-image: url(<?php echo $art['image']; ?>);"></div>
+                            <div class="article__info">
+                                <a href="article.php?id=<?php echo $art['id'];?>"><?php echo $art['title']; ?></a>
+                                <div class="article__info__meta">
+                                    <?php
+
+                                    $art_cat = false;
+                                    foreach ($categories as $cat)
+                                    {
+                                        if($cat['id']==$art['categorie_id'])
+                                        {
+                                            $art_cat = $cat;
+                                            break;
+                                        }
+                                    }
+                                    ?>
+
+                                    <small>Категория: <a href="article.php?categorie=<?php echo $art_cat['id'];?>">
+                                            <?php echo $art_cat['title']; ?>
+                                        </a></small>
+                                </div>
+                                <div class="article__info__preview"><?php echo mb_substr(strip_tags($art['text']), 0, 100,'utf-8').' ...'; ?></div>
+                            </div>
+                        </article>
+
+                    <?php
+                    }
+                    ?>
+                </div>
+              </div>
+            </div>
+
+            <div class="block">
+              <a href="article.php?categorie=1">Все записи</a>
+              <h3>Свежие статьи о RPG</h3>
+              <div class="block__content">
+                <div class="articles articles__horizontal">
+
+                    <?php $articles = mysqli_query($connection, "SELECT * FROM `articles` WHERE `categorie_id`='1' ORDER BY `pubdate` DESC LIMIT 4");
+
+                    while($art = mysqli_fetch_assoc($articles))
+                    {
+                        ?>
+
+                        <article class="article">
+                            <div class="article__image" style="background-image: url(<?php echo $art['image']; ?>);"></div>
+                            <div class="article__info">
+                                <a href="article.php?id=<?php echo $art['id'];?>"><?php echo $art['title']; ?></a>
+                                <div class="article__info__meta">
+                                    <?php
+
+                                    $art_cat = false;
+                                    foreach ($categories as $cat)
+                                    {
+                                        if($cat['id']==$art['categorie_id'])
+                                        {
+                                            $art_cat = $cat;
+                                            break;
+                                        }
+                                    }
+                                    ?>
+
+                                    <small>Категория: <a href="article.php?categorie=<?php echo $art_cat['id'];?>">
+                                            <?php echo $art_cat['title']; ?>
+                                        </a></small>
+                                </div>
+                                <div class="article__info__preview"><?php echo mb_substr(strip_tags($art['text']), 0, 100,'utf-8').' ...'; ?></div>
+                            </div>
+                        </article>
+
+                        <?php
+                    }
+                    ?>
+
+                </div>
+              </div>
+            </div>
+
+            <div class="block">
+              <a href="article.php?categorie=4">Все записи</a>
+              <h3>Свежие статьи о Action-RPG</h3>
+              <div class="block__content">
+                <div class="articles articles__horizontal">
+
+                    <?php $articles = mysqli_query($connection, "SELECT * FROM `articles` WHERE `categorie_id`='4' ORDER BY `pubdate` DESC LIMIT 4");
+
+                    while($art = mysqli_fetch_assoc($articles))
+                    {
+                        ?>
+
+                        <article class="article">
+                            <div class="article__image" style="background-image: url(<?php echo $art['image']; ?>);"></div>
+                            <div class="article__info">
+                                <a href="article.php?id=<?php echo $art['id'];?>"><?php echo $art['title']; ?></a>
+                                <div class="article__info__meta">
+                                    <?php
+
+                                    $art_cat = false;
+                                    foreach ($categories as $cat)
+                                    {
+                                        if($cat['id']==$art['categorie_id'])
+                                        {
+                                            $art_cat = $cat;
+                                            break;
+                                        }
+                                    }
+                                    ?>
+
+                                    <small>Категория: <a href="article.php?categorie=<?php echo $art_cat['id'];?>">
+                                            <?php echo $art_cat['title']; ?>
+                                        </a></small>
+                                </div>
+                                <div class="article__info__preview"><?php echo mb_substr(strip_tags($art['text']), 0, 100,'utf-8').' ...'; ?></div>
+                            </div>
+                        </article>
+
+                        <?php
+                    }
+                    ?>
+
+                </div>
+              </div>
+            </div>
+          </section>
+          <section class="content__right col-md-4">
+              <?php include "includes/sidebar.php"?>
+          </section>
+        </div>
+      </div>
+    </div>
+
+    <?php include "includes/footer.php"?>
+
+  </div>
+
+</body>
+</html>
